b6fb1eb22b067a7f3c98acb86532e0129c8d8463
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-01-06 23:28:26 +0600

    Add files via upload

---
+++ ExamoleJava2.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.examplejava;
+
+public class ExamoleJava2 {
+    public static void main(String[] args){
+        System.out.println("ID: 101");
+        System.out.println("tittle: Iphone15");
+        System.out.println("Price: 1820 euros");
+        System.out.println("catagory:Phone");
+    }
+    
+}
