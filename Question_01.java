7644855d27d601984176b4b5aa8854e997410539
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-05-30 22:23:38 +0600

    Add files via upload

---
+++ Question_01.java
@@ -0,0 +1,22 @@
+
+package mollah.random.java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Question_01 {
+public static void main(String[] args) {
+String fileName = "sajib.sh";
+try {
+File file = new File(fileName);
+Scanner scanner = new Scanner(file);
+while (scanner.hasNextLine()) {
+String line = scanner.nextLine();
+System.out.println(line);
+}
+scanner.close();
+} catch (FileNotFoundException e) {
+System.err.println("Error: The file " + fileName + " was not found.");
+}
+}
+}
